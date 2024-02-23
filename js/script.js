8d2c76ee2689632589988e738f50b097c62a808b
patricklohn <patricklohn3@gmail.com>
2024-02-22 23:20:55 -0300

    settingsVersion01

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const qrCodeBtn = document.querySelector("#btn-qr");
 const inputValue = document.querySelector("#qr-form input");
 const qrCodeImg = document.querySelector("#qrImg-code");
 const selectCode = document.querySelector("#selectCode");
-const aconrBtn = document.querySelector("#btn-dow");
 
 /*Gerar codigos QR*/
 function generateQrCode(){
@@ -18,17 +17,14 @@ function generateQrCode(){
         case "qr":
         qrCodeBtn.innerText = "Gerando código...";
         qrCodeImg.src = `http://barcode.orcascan.com/?type=qr&data=${qrCodeInputValue}`;
-        aconrBtn.href = `http://barcode.orcascan.com/?type=qr&data=${qrCodeInputValue}&format=png`;
         break
         case "code128":
         qrCodeBtn.innerText = "Gerando código...";
         qrCodeImg.src = `http://barcode.orcascan.com/?type=code128&data=${qrCodeInputValue}`;
-        aconrBtn.href = `http://barcode.orcascan.com/?type=code128&data=${qrCodeInputValue}&format=png`;
         break
         case "ean13":
         qrCodeBtn.innerText = "Gerando código...";
         qrCodeImg.src = `http://barcode.orcascan.com/?type=ean13&data=${qrCodeInputValue}`;
-        aconrBtn.href = `http://barcode.orcascan.com/?type=ean13&data=${qrCodeInputValue}&format=png`;
         break
     }
 
